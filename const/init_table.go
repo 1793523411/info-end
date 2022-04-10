60e265a5de22e55c119669119a87e904b54fd7ce
yang guo jie <yangguojie.ygj@bytedance.com>
2022-04-10 12:33:41 +0800

    feat: 添加文件

---
+++ const/init_table.go
@@ -0,0 +1,15 @@
+package allconst
+
+var Tables = map[string]string{
+	"userInfo":     "userInfo",
+	"avator":       "avator",
+	"video":        "video",
+	"video_img":    "video_img",
+	"video_record": "video_record",
+	"topic_record": "topic_record",
+}
+
+var Table_index = map[string]string{
+	"video_record": "video_record_index",
+	"topic_record": "topic_record_index",
+}
